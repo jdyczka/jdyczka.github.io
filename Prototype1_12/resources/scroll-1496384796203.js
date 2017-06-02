14a88860a7615cb3634d99f88be022d6bb66e910
jdyczka <asia.dyczka@gmail.com>
2017-06-02 08:27:28 +0200

    prototype test init

---
+++ Prototype1_12/resources/scroll-1496384796203.js
@@ -0,0 +1,16 @@
+(function(window, undefined) {
+
+    /*********************** START STATIC ACCESS METHODS ************************/
+
+    jQuery.extend(jimMobile, {
+        "loadScrollBars": function() {
+            jQuery(".s-d12245cc-1680-458d-89dd-4f0d7fb22724 .ui-page").overscroll({ showThumbs:true, direction:'vertical' });
+            jQuery(".s-d12245cc-1680-458d-89dd-4f0d7fb22724 #s-Panel_11").overscroll({ showThumbs:false, direction:'horizontal' });
+            jQuery(".s-d12245cc-1680-458d-89dd-4f0d7fb22724 #s-utwory").overscroll({ showThumbs:false, direction:'vertical' });
+            jQuery(".s-d12245cc-1680-458d-89dd-4f0d7fb22724 #s-artysci").overscroll({ showThumbs:false, direction:'vertical' });
+         }
+    });
+
+    /*********************** END STATIC ACCESS METHODS ************************/
+
+}) (window);
